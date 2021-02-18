10686b66d5ed6e4ad2027d6d3d22cd6cc977dc03
masami10 <499397328@qq.com>
2021-02-18 22:10:59 +0800

    [IMP] 完善python2和python3依赖兼容问题

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     install_requires=[          # 添加了依赖的 package
-        'numpy>=1.18.2'
+        'numpy>=1.18.2; python_version > "3.5"',
+        'numpy<=1.16.6; python_version <= "3.5"',
     ]
 )
