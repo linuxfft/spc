14d98d708a382c01fc26fba0f5f76911efce00ec
masami10 <499397328@qq.com>
2021-02-19 09:38:06 +0800

    [FIX] (python2 comp) 兼容python2 需要手动安装typing模块

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     install_requires=[          # 添加了依赖的 package
         'numpy>=1.18.2; python_version > "3.5"',
         'numpy<=1.16.6; python_version <= "3.5"',
+        'typing;python_version<"3.5"',
     ]
 )
