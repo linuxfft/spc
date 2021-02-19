74e9d927496d154caf4e190c5c0d1d6ff3df9800
masami10 <499397328@qq.com>
2021-02-19 14:54:20 +0800

    [REN] spc -> lexen_spc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lexen-spc',
+    name='lexen_spc',
     packages=find_packages(),
     version='0.1.0',
     install_requires=[          # 添加了依赖的 package
