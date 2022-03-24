a18423872660ee2c3c643e5aafc965cda9feebce
kevin0120 <jingwei1224@163.com>
2022-03-24 16:55:40 +0800

    SPC XBAR-SBAR功能修复，并测试通过

--- src/spc.cpp
+++ src/spc.cpp
@@ -22,8 +22,9 @@ using namespace nc;
  * 连续每n的数据的均值的变化图
  * 可以直观反应设备的稳定性，以及性能的实时变化。
  */
+ //todo 考虑传入一个一维数组data,在函数内部进行拆分成二维数组
 ST_RET xbarSbar(double **data, size_t n_col, size_t n_row, SPC_RET **spc_ret) {
-    if (n_row < 2 || n_row > 10) {
+    if (n_col < 2 || n_col > 10) {
         return ERROR_DATA_SIZE;
     }
 
@@ -69,7 +70,7 @@ ST_RET xbarSbar(double **data, size_t n_col, size_t n_row, SPC_RET **spc_ret) {
 
     double lclx = xbar_ret.data - A3[n_col] * sbar_ret.data;
     double uclx = xbar_ret.data + A3[n_col] * sbar_ret.data;
-
+    memset(spc_ret, 0, sizeof(SPC_RET));
     memcpy_spc_ret(spc_ret, X, n_row);
 
     SPC_RET *pRet = *spc_ret;
